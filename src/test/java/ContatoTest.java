aafaf11e8d1854c884050762c9ab0a65052be7e1
José Ryan <joseryan695@gmail.com>
2024-08-13 09:20:31 -0300

    adicionado teste ContatoTest.java para o construtor da classe Contato.java

---
+++ src/test/java/ContatoTest.java
@@ -0,0 +1,15 @@
+import entities.Contato;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class ContatoTest {
+
+    @Test
+    public void testaConstrutor(){
+        Contato c1 = new Contato("ze", 26, 07);
+        assertEquals("ze", c1.getNome());
+        assertEquals(26, c1.getDiaAniversario());
+        assertEquals(07, c1.getMesAniversario());
+    }
+}
